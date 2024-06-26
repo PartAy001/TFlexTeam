a5c633e8d18a2f690a262cd7b9389d65d041fa44
Part A-y <168458141+PartAy001@users.noreply.github.com>
2024-06-26 09:40:34 +0000

    아 프로그래머 되지 말고 화학교사로 진로 틀까...

--- .ccls-cache/@home@runner@TFlexTeam/temp.h
+++ .ccls-cache/@home@runner@TFlexTeam/temp.h
@@ -5,6 +5,7 @@
 #include <cassert>
 
 #define ELIF else if
+#define elif ELIF
 
 template <typename T> union Space<T> newSpace(T data) {
   union Space<T> ret;
@@ -43,24 +44,29 @@ Spaces<T, TypeFileStrc>::transform() {
       .FILE: //일단은 크게는 문제는 없다, 근데 예, FILESTRC랑 길이가 같다...
              //후...., ㅈㄴ어렵개 설계했네, 과거의 나 그저 G.O.A.T..... 이딴걸
              //쉬운거 취급하다니...
-    if (std::is_same_v<T, TypeFileStrc> && this.isLoad){
-        this.Dlibs.Functions.FileNFileStrc.FILE2FILESTRC();
-      } else {
-        this.Dlibs.Functions.FileNFileStrc.FILESTRC2FILE();
-      };
-    } elif ()
-    break;
-
-  case this.Dlibs.faketypes
-      .F: //기준을 모르겠다 ㅠㅠ, 분명히 로드시에 구분이 되야하는데.., 에초에
-          //크기비교가 아니었다는 증거다 이건.
-    if (this.isLoad) {
+    if (std::is_same_v<T, TypeFileStrc> && this.isLoad) {
       this.Dlibs.Functions.FileNFileStrc.FILE2FILESTRC();
-    } else {
+    }
+    elif (std::is_same_v<T, TypeFileStrc>) {
+      this.Dlibs.Functions.FileNFileStrc.FILESTRC2FILE();
+    }
+    elif (this.isLoad) { this.Dlibs.Functions.FileNFileStrc.FILE2FILESTRC(); }
+    else {
       this.Dlibs.Functions.FileNFileStrc.FILESTRC2FILE();
     };
-    break;
   }
+
+case 
+
+  // case this.Dlibs.faketypes
+  //     .F: //기준을 모르겠다 ㅠㅠ, 분명히 로드시에 구분이 되야하는데.., 에초에
+  //         //크기비교가 아니었다는 증거다 이건.
+  //   if (this.isLoad) {
+  //     this.Dlibs.Functions.FileNFileStrc.FILE2FILESTRC();
+  //   }
+  //   break;
+  // }
+  // ㄹㅇㅋㅋ (실성)
 }
 template <typename T, typename TypeFileStrc>
 Spaces<T, TypeFileStrc> Spaces<T, TypeFileStrc>::rm() {
@@ -90,7 +96,8 @@ Spaces<T, TypeFileStrc> Spaces<T, TypeFileStrc>::shr(size_t N) {
 //???
 
 template <typename T, typename TypeFileStrc>
-template <typename T2> Spaces<T2, TypeFileStrc> Spaces<T, TypeFileStrc>::PillingUpSpace();
-//???
+template <typename T2>
+Spaces<T2, TypeFileStrc> Spaces<T, TypeFileStrc>::PillingUpSpace();
+  //???
 
 #endif
